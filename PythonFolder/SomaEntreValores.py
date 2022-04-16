5c525c1bae0aa52c728b527c1616f29090396393
Yuri Nunes <inv72kmaster@gmail.com>
2022-04-15 23:59:13 -0300

    print de prograna

---
+++ PythonFolder/SomaEntreValores.py
@@ -0,0 +1,19 @@
+n1 = int(input('Numero 1 '))
+n2 = int(input('Numero 2 '))
+var1 = ('digite algo: ')
+s = n1 + n2
+#print (f'A soma vale entre { n1} e { n2} é {s}')
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
+
+print(var1.isalnum())
+
+
+
+
+
+
+
+
+
+
+
